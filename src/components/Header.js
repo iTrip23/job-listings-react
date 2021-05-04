e829d75697669cd266784141174f29fc65c60e0c
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-05-04 21:32:10 +0100

    made the header changes in mobile version

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import './Header.css';
 
-const Header = () => {
-	// let screenWidthEl;
-	// window.addEventListener('resize', () => {
-	// 	screenWidthEl = window.innerWidth;
-	// })
-	return (
-		<header>
-			<img src='./images/bg-header-desktop.svg' alt='header background' className='background-img' />
-		</header>
-	)
-}
+const Header = () => (
+	<header className='background-img'>
+	</header >
+)
 
 export default Header;
